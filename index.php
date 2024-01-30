9c4657626a7baeba40cdde2ab0af817bbd3b7d5d
Frz3108 <145887257+Frz3108@users.noreply.github.com>
2024-01-30 10:39:01 +0700

    index.php

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Data 2</title>
+    <title>Data Server 2</title>
 </head>
 <body>
     <?php
-        echo 'DATA 2';
+        echo 'DATA SERVER 2';
     ?>
 </body>
 </html>
