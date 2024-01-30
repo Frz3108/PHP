c8106647f8cb4f8d71475a6ad9b8efe13812a4c5
Frz3108 <145887257+Frz3108@users.noreply.github.com>
2024-01-30 10:37:45 +0700

    PHP
    PHP DATA SERVER 2

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Data 2</title>
+</head>
+<body>
+    <?php
+        echo 'DATA 2';
+    ?>
+</body>
+</html>
